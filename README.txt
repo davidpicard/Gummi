d3842187d07e72e307adea080a0cd3c9527b8632
Alexander van der Mey <alexvandermey@gmail.com>
2009-08-06 14:53:36 +0000

    importing distutils files
    git-svn-id: http://svn.midnightcoding.org/gummi/trunk@8 6e728def-ac18-4b39-bad8-e7d984b3b6fa

---
+++ README.txt
@@ -0,0 +1,10 @@
+======================================================
+	Gummi - latex editor for GTK
+======================================================
+
+Latest news, releases and bug reports can be found on:
+
+    http://code.google.com/p/gummi
+
+
+
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import sys
+
+_data_files = [
+	('share/applications', ['gummi/misc/gummi.desktop']),
+	('share/pixmaps', ['gummi/misc/gummi.png'])	
+	]
+
+files = ["gummi.xml",
+	 "docs/CHANGES", 
+	 "docs/LICENSE",
+	 "gui/gummi.xml",
+	 "gui/prefs.xml",
+	 "gui/icon.png"
+	]
+
+setup(
+	name = 'gummi',
+	version = '0.1beta',
+	summary = 'Simple LaTeX editor',
+	description = 'Simple LaTex editor for GTK users',
+	author = 'Alexander van der Mey',
+	author_email = 'alexvandermey@gmail.com',
+	url = 'http://code.google.com/p/gummi/',
+	license = 'GPLv2',
+    	packages = ['gummi'],
+	package_data = {'gummi' : files },
+	scripts = ['gummi/misc/gummi'],
+	data_files = _data_files,
+)
+
