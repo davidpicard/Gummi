99e92a9820d8b6e1a092bcf733427987b98c039d
Alexander van der Mey <alexvandermey@gmail.com>
2009-08-06 14:46:39 +0000

    importing python code
    git-svn-id: http://svn.midnightcoding.org/gummi/trunk@4 6e728def-ac18-4b39-bad8-e7d984b3b6fa

---
+++ gummi/prefs.py
@@ -0,0 +1,132 @@
+
+# --------------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <alexvandermey@gmail.com> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return -Alexander van der Mey
+# --------------------------------------------------------------------------------
+
+import os
+import sys
+import gtk
+import gconf
+
+WELCOME = """\\documentclass{article}
+\\begin{document}
+
+\\noindent\huge{Welcome to Gummi} \\\\
+\\\\
+\\large	{You are using the svn version.\\\\
+	I welcome your suggestions at:\\\\
+	http://code.google.com/p/gummi\\\\
+
+\\end{document}"""
+	
+
+class prefs:
+	
+	def __init__(self, parent):
+		self.parent = parent
+		self.gconf_client = gconf.client_get_default()
+		self.check_for_defaults()
+
+	def create_gui(self):
+		builder = gtk.Builder()	
+		builder.add_from_file(self.parent.installdir + "/gui/prefs.xml")
+
+		self.prefwindow = builder.get_object("prefwindow")
+		self.box = builder.get_object("hbox1")
+		self.notebook = builder.get_object("notebook1")
+		self.textwrap_char = builder.get_object("textwrap_char")
+		self.textwrap_word = builder.get_object("textwrap_word")
+		self.textlines_display = builder.get_object("textlines_display")
+		self.textlines_highlight = builder.get_object("textlines_highlight")
+		self.box.pack_start(self.notebook, expand=False)
+		
+		self.textwrap_char.connect("toggled", self.run_wrapping_char, "textwrap char")
+		self.textwrap_word.connect("toggled", self.run_wrapping_word, "textwrap word")
+		self.textlines_display.connect("toggled", self.run_lines, "textlines display")
+		self.textlines_highlight.connect("toggled", self.run_highlight, "textlines highlight")
+
+		self.get_current_status()
+
+		builder.connect_signals(self)
+		self.prefwindow.show_all()
+
+
+	def get_current_status(self):
+		self.textwrap_char.set_active(1)
+		if self.get_config_value("text_wrapping") == "char":
+			self.textwrap_char.set_active(1)
+		if self.get_config_value("text_wrapping") == "word":
+			self.textwrap_char.set_active(1)
+			self.textwrap_word.set_active(1)
+		if self.get_config_value("text_lines") == "True":
+			self.textlines_display.set_active(1)
+		if self.get_config_value("text_highlight") == "True":
+			self.textlines_highlight.set_active(1)
+
+
+	def run_wrapping_char(self, widget, data=None):
+		if widget.get_active() == False:
+			self.textwrap_word.set_active(0)
+			self.parent.editorpane.set_wrapping("None")
+		else:
+			self.parent.editorpane.set_wrapping("char")
+
+
+	def run_wrapping_word(self, widget, data=None):
+		if widget.get_active() == False:
+			self.parent.editorpane.set_wrapping("char")
+		if widget.get_active() == True:
+			self.parent.editorpane.set_wrapping("word")
+
+
+	def run_lines(self, widget, data=None):
+		if widget.get_active() == False:
+			self.parent.editorpane.editorview.set_show_line_numbers(False)
+			self.gconf_client.set_string("/apps/gummi/text_lines", "False")
+		else:
+			self.parent.editorpane.editorview.set_show_line_numbers(True)
+			self.gconf_client.set_string("/apps/gummi/text_lines", "True")
+
+
+	def run_highlight(self, widget, data=None):
+		if widget.get_active() == False:
+			self.parent.editorpane.editorview.set_highlight_current_line(False)
+			self.gconf_client.set_string("/apps/gummi/text_highlight", "False")
+		else:
+			self.parent.editorpane.editorview.set_highlight_current_line(True)
+			self.gconf_client.set_string("/apps/gummi/text_highlight", "True")
+
+
+	def get_config_value(self, type, item):
+		if type == "string":
+			configitem = self.gconf_client.get_string("/apps/gummi/" + item)
+			return configitem
+		if type == "list":
+			configitem = self.gconf_client.get_string("/apps/gummi/" + item)
+			return configitem
+
+
+	def get_config_status(self, item):
+		if self.gconf_client.get_string("/apps/gummi/" + item) == "True":
+			return 1
+		else:
+			return 0
+	
+
+	def check_for_defaults(self):
+		textwrapping = self.gconf_client.get_string("/apps/gummi/text_wrapping")
+		if textwrapping == None:
+			self.gconf_client.set_string("/apps/gummi/text_wrapping", "word")
+		textlines = self.gconf_client.get_string("/apps/gummi/text_lines")
+		if textlines == None:
+			self.gconf_client.set_string("/apps/gummi/text_lines", "True")
+		texthighlight = self.gconf_client.get_string("/apps/gummi/text_highlight")
+		if texthighlight == None:
+			self.gconf_client.set_string("/apps/gummi/text_highlight", "True")
+		default = self.gconf_client.get_string("/apps/gummi/default-text")		
+		if default == None:
+			self.gconf_client.set_string("/apps/gummi/default-text", WELCOME)
+
---
+++ gummi/texpane.py
@@ -0,0 +1,72 @@
+
+# --------------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <alexvandermey@gmail.com> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return -Alexander van der Mey
+# --------------------------------------------------------------------------------
+
+import os
+import sys
+import gtk
+import gtksourceview2
+import pango
+import gobject
+from datetime import date, datetime
+
+
+class texpane:
+
+	def __init__(self):
+
+		self.bufferS = gtksourceview2.Buffer()
+		self.manager = gtksourceview2.LanguageManager()
+		self.language = self.manager.get_language("latex")
+		self.bufferS.set_language(self.language)
+		self.bufferS.set_highlight_matching_brackets(True)
+		self.bufferS.set_highlight_syntax(True)
+		self.editorview = gtksourceview2.View(self.bufferS)
+
+		#todo: replace with gconf values from prefs
+		self.editorview.set_show_line_numbers(True)
+		self.editorview.set_highlight_current_line(True)
+		self.editorview.set_wrap_mode(gtk.WRAP_WORD)
+		
+		self.editorview.modify_font(pango.FontDescription("monospace 10"))
+		
+		self.textchange = datetime.now()
+		self.prevchange = datetime.now()
+		self.check_text_change()
+
+		self.editorview.connect("key-press-event", self.set_text_change,)
+		self.bufferS.set_modified(False)
+
+		self.pdffile = None
+		self.workfile = None
+		self.filename = None
+		self.status = 1
+
+
+	def set_wrapping(self, wrapmode):
+		if wrapmode == "None":
+			self.editorview.set_wrap_mode(gtk.WRAP_NONE)
+		elif wrapmode == "char":
+			self.editorview.set_wrap_mode(gtk.WRAP_CHAR)
+		else:
+			self.editorview.set_wrap_mode(gtk.WRAP_WORD)
+
+
+	def set_text_change(self, widget, event):
+		self.bufferS.set_modified(True)
+		self.textchange = datetime.now()
+
+
+	def check_text_change(self):	
+		if self.prevchange != self.textchange:
+			self.prevchange = self.textchange
+			return True
+		else:		
+			return False
+
+
+
