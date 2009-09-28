dcdeaffbb53cbd43e709ea995ac6071a2ef91328
Alexander van der Mey <alexvandermey@gmail.com>
2009-09-28 12:12:31 +0000

    import panels are now parentless + smarter switching
    git-svn-id: http://svn.midnightcoding.org/gummi/trunk@121 6e728def-ac18-4b39-bad8-e7d984b3b6fa

--- gummi/GummiGUI.py
+++ gummi/GummiGUI.py
@@ -63,6 +63,8 @@ class GummiGUI:
 		self.table_rows = builder.get_object("table_rows")
 		self.table_cols = builder.get_object("table_cols")
 
+		self.import_box = builder.get_object("import_box")
+
 		self.tempdir = os.environ.get("TMPDIR", "/tmp")
 
 		self.config = Preferences.Preferences(self)
@@ -186,15 +188,11 @@ class GummiGUI:
 
 	def on_import_tabs_switch_page(self, notebook, page, page_num):
 		newactive = notebook.get_nth_page(page_num).get_name()
+		self.import_box.foreach(lambda x:self.import_box.remove(x))
 		if newactive == "box_image": 
-			self.image_pane.show()
-			self.table_pane.hide()
-		if newactive == "box_minimize": 
-			self.image_pane.hide()
-			self.table_pane.hide()
+			self.import_box.add(self.image_pane)
 		if newactive == "box_table": 
-			self.image_pane.hide()
-			self.table_pane.show()
+			self.import_box.add(self.table_pane)
 			self.table_cols.set_value(3)
 			self.table_rows.set_value(3)
 
