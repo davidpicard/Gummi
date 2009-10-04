041e58ef11e370e46a9b07386ec416a8b55947fa
alexandervdm <alexandervdm@6e728def-ac18-4b39-bad8-e7d984b3b6fa>
2009-10-04 07:15:32 +0000

    dropped beerware license, mit license remains

--- gummi/GummiGUI.py
+++ gummi/GummiGUI.py
@@ -1,10 +1,23 @@
 
-# --------------------------------------------------------------------------------
-# "THE BEER-WARE LICENSE" (Revision 42):
-# <alexvandermey@gmail.com> wrote this file. As long as you retain this notice you
-# can do whatever you want with this stuff. If we meet some day, and you think
-# this stuff is worth it, you can buy me a beer in return -Alexander van der Mey
-# --------------------------------------------------------------------------------
+# Copyright (c) 2009 Alexander van der Mey <alexvandermey@gmail.com>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 import os
 import sys
@@ -237,6 +250,7 @@ class GummiGUI:
 		about_dialog.set_transient_for(self.mainwindow)
 		about_dialog.set_destroy_with_parent(True)
 		about_dialog.set_name("Gummi")
+		about_dialog.set_license(Preferences.LICENSE)
 		about_dialog.set_version(Preferences.VERSION)
 		about_dialog.set_copyright("Copyright \xc2\xa9 2009 Alexander van der Mey")
 		about_dialog.set_website("http://gummi.googlecode.com")
