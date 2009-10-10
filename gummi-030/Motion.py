490721267d72178a063f742dd4e755935533e0c1
Alexander van der Mey <alexvandermey@gmail.com>
2009-10-10 20:48:07 +0000

    branching the 0.3b release
    git-svn-id: http://svn.midnightcoding.org/gummi/trunk@211 6e728def-ac18-4b39-bad8-e7d984b3b6fa

---
+++ gummi-030/Motion.py
@@ -0,0 +1,124 @@
+
+# --------------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <alexvandermey@gmail.com> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return -Alexander van der Mey
+# --------------------------------------------------------------------------------
+
+import os
+import sys
+import gtk
+import thread
+import shutil
+import subprocess
+import traceback
+import tempfile
+import gobject
+import time
+
+import Preferences
+
+
+class Motion:
+	
+	def __init__(self, tex, pdf, error, light):
+		self.editorpane = tex
+		self.previewpane = pdf
+		self.statuslight = light
+
+		self.texfile = None		
+		self.tmpfile = None
+		self.pdffile = None
+		self.status = 1
+
+		self.editorviewer = self.editorpane.editorviewer
+		self.editorbuffer = self.editorpane.editorbuffer
+		self.errorbuffer = error.get_buffer()
+
+		gobject.threads_init()
+		gtk.gdk.threads_init()
+		self.start_monitoring()
+
+	def start_monitoring(self):
+		self.refresh = thread.start_new_thread(self.update_preview, ())
+
+	def create_environment(self, filename):	
+		self.texfile = filename
+		self.texpath = os.path.dirname(self.texfile) + "/"	
+		if ".tex" in self.texfile:
+			self.texname = os.path.basename(self.texfile)[:-4] 
+		else:
+			self.texname = os.path.basename(self.texfile)
+		fd, path = tempfile.mkstemp(".tex")
+		self.workfile = os.readlink("/proc/self/fd/%d" % fd)
+		self.pdffile = self.workfile[:-4] + ".pdf"
+		print ("\nEnvironment created for: \nTEX: " + self.texfile + 
+		       "\nTMP: " + self.workfile + "\nPDF: " + self.pdffile + "\n")
+		self.initial_preview()
+
+	def initial_preview(self):
+		self.update_workfile()
+		self.update_pdffile()
+		try:		
+			self.previewpane.create_preview(self.pdffile)
+			self.previewpane.refresh_preview()
+		except:
+			self.previewpane.drawarea.hide()
+
+	def export_pdffile(self):
+		try: # export the pdf file if one exists
+			export = self.texpath + self.texname + ".pdf"
+			shutil.copy2(self.pdffile, export)
+			os.chdir(self.texpath)
+		except IOError: pass
+
+	def update_workfile(self):
+		try:
+			# these two lines make the program hang in certain situations, no clue why	
+			#self.editorpane.editorview.set_sensitive(False)
+			self.buffer = self.editorpane.editorviewer.get_buffer()
+			start_iter, end_iter = self.buffer.get_start_iter(), self.buffer.get_end_iter()
+			content = self.buffer.get_text(start_iter, end_iter)
+			#self.editorpane.editorview.set_sensitive(True)
+			tmpmake = open(self.workfile, "w")
+			tmpmake.write(content)
+			tmpmake.close()
+			self.editorviewer.grab_focus() #editorpane regrabs focus
+		except:
+			print traceback.print_exc()
+
+	def update_pdffile(self):	
+		#os.chdir(self.texpath)
+		pdfmaker = subprocess.Popen('pdflatex -interaction=nonstopmode --output-directory=/tmp/ "%s"' % (self.workfile), shell=True, stdin=None, stdout = subprocess.PIPE, stderr=None)
+		output = pdfmaker.communicate()[0]
+		pdfmaker.wait()
+		try: os.close(3)
+		except: pass	
+		self.errorbuffer.set_text(output)
+		err1 = "Fatal error"
+		err2 = "Emergency stop"
+		err3 = "LaTeX Error"
+		if err1 in output or err2 in output or err3 in output:
+			self.statuslight.set_stock_id("gtk-no")
+		else:
+			self.statuslight.set_stock_id("gtk-yes")
+		
+
+	def update_preview(self):
+		while True:
+			try:
+				if self.previewpane and self.status == 1 and self.editorpane.check_text_change():
+					gtk.gdk.threads_enter
+					self.editorpane.check_text_change()
+					self.update_workfile()
+					self.update_pdffile()
+					self.previewpane.refresh_preview()			
+					gtk.gdk.threads_leave
+			except:
+				print "something is wrong with the refresh thread"
+				print traceback.print_exc()
+			time.sleep(1.0)
+
+
+
---
+++ gummi-030/PdfPane.py
@@ -0,0 +1,103 @@
+
+# --------------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <alexvandermey@gmail.com> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return -Alexander van der Mey
+# --------------------------------------------------------------------------------
+
+
+import gtk
+import poppler
+import os.path
+
+ORIGINAL_HEIGHT = 841.89 
+ORIGINAL_WIDTH  = 595.276 
+
+
+class PdfPane:
+
+	def __init__(self, drawarea):
+		self.drawarea = drawarea
+		self.scale = 1
+		self.maxscale = 1.6 
+		self.minscale = 1.0
+		self.page_displayed = 0
+		self.page_total = None
+
+	def create_preview(self, pdffile):
+		self.pdffile = pdffile	
+		self.uri = "file://" + pdffile
+		self.document = poppler.document_new_from_file(self.uri, None)
+		self.page_total = self.document.get_n_pages()
+		self.current_page = self.document.get_page(0)
+		self.width, self.height = self.current_page.get_size()
+		self.drawarea.set_size_request(int(self.width), int(self.height))
+		self.drawarea.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(6400, 6400, 6440))
+		self.drawarea.connect("expose-event", self.on_expose)
+
+	def refresh_preview(self):
+		if os.path.exists(self.pdffile): # only attempt refresh if file exists
+			self.drawarea.show()
+			self.uri = "file://" + self.pdffile
+			self.document = poppler.document_new_from_file(self.uri, None)
+			self.page_total = self.document.get_n_pages()
+			if self.page_total <= (self.page_displayed):		
+				self.current_page = self.document.get_page(self.page_total - 1)			
+			else:		
+				self.current_page = self.document.get_page(self.page_displayed)
+			self.drawarea.queue_draw()
+
+	def jump_to_nextpage(self):
+		if (self.page_displayed + 1) < self.page_total:
+			self.page_displayed = self.page_displayed + 1
+			self.uri = "file://" + self.pdffile
+			self.document = poppler.document_new_from_file(self.uri, None)
+			self.page_total = self.document.get_n_pages()
+			self.current_page = self.document.get_page(self.page_displayed)
+			self.drawarea.queue_draw()
+		
+	def jump_to_prevpage(self):
+		if (self.page_displayed - 1) >= 0:
+			self.page_displayed = self.page_displayed - 1
+			self.uri = "file://" + self.pdffile
+			self.document = poppler.document_new_from_file(self.uri, None)
+			self.page_total = self.document.get_n_pages()
+			self.current_page = self.document.get_page(self.page_displayed)
+			self.drawarea.queue_draw()
+
+	def zoom_in_pane(self):
+		if self.scale < self.maxscale:
+			self.scale = self.scale + 0.1
+			self.width = (self.width * 1.1)
+			self.height = (self.height * 1.1)
+			self.drawarea.set_size_request(int(self.width), int(self.height))
+			self.refresh_preview()
+
+	def zoom_out_pane(self):
+		if self.scale > self.minscale:
+			self.scale = self.scale - 0.1
+			self.width = (self.width / 1.1) 
+			self.height = (self.height / 1.1) 
+			self.drawarea.set_size_request(int(self.width), int(self.height))
+			self.refresh_preview()
+
+	def zoom_normal_pane(self):
+		self.scale = 1
+		self.height = ORIGINAL_HEIGHT
+		self.width = ORIGINAL_WIDTH
+		self.drawarea.set_size_request(int(self.width), int(self.height))
+		self.refresh_preview()
+
+	def on_expose(self, widget, event):
+		cr = widget.window.cairo_create()
+		cr.set_source_rgb(1, 1, 1)
+		cr.translate(0, 0)
+		if self.scale != 1:
+			cr.scale(self.scale, self.scale)
+		cr.rectangle(0, 0, self.width, self.height)
+		cr.fill()
+		self.current_page.render(cr)
+
+
+
