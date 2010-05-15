0814ca1af722896fe1d59e5a726a670700a68236
Alexander van der Mey <alexvandermey@gmail.com>
2010-05-15 15:02:42 +0000

    removed some constructor clutter
    git-svn-id: http://svn.midnightcoding.org/gummi/trunk@319 6e728def-ac18-4b39-bad8-e7d984b3b6fa

--- gummi/Core.py
+++ gummi/Core.py
@@ -26,13 +26,13 @@ import os
 import sys
 import traceback
 
-import GummiGUI
-import Preferences
-import Motion
-import TexPane
-import PreviewPane
-import IOFunctions
-import Biblio
+from Biblio import Biblio
+from GummiGUI import MainGUI
+from IOFunctions import IOFunctions
+from Motion import Motion
+from Preferences import Preferences
+from PreviewPane import PreviewPane
+from TexPane import TexPane
 
 
 class Core:
@@ -45,30 +45,31 @@ class Core:
 		self.filename = None
 
 		# graphical components:
-		self.builder = gtk.Builder()
-		self.builder.add_from_file(path + "/gui/gummi.glade")
-		statusbar = self.builder.get_object("statusbar")
+		builder = gtk.Builder()
+		builder.add_from_file(path + "/gui/gummi.glade")
+		statusbar = builder.get_object("statusbar")
 
 		# class instances:
-		config = Preferences.Preferences()
-		self.editorpane = TexPane.TexPane(config)
-		self.previewpane = PreviewPane.PreviewPane(self.builder)
-		self.motion = Motion.Motion(config, self.editorpane, self.previewpane, self.builder)
-		self.iofunc = IOFunctions.IOFunctions(config, statusbar, self.editorpane, self.motion)
-		self.biblio = Biblio.Biblio(self.editorpane, self.motion)
-		self.gui = GummiGUI.MainGUI(self, config, self.iofunc, self.biblio, self.motion)
+		config = Preferences(path)
+		editorpane = TexPane(config)
+		previewpane = PreviewPane(builder)
+		motion = Motion(config, editorpane, previewpane, builder)
+		iofunc = IOFunctions(config, statusbar, editorpane, motion)
+		biblio = Biblio(editorpane, motion)
+		gui = MainGUI \
+			(config, builder, iofunc, biblio, motion, editorpane, previewpane)
 
 		# setup document to load:
 		if len(sys.argv) > 1:
 			self.filename = sys.argv[1]
-			self.iofunc.load_file(self.filename)
+			iofunc.load_file(self.filename)
 		else:
 			self.filename = None
-			self.iofunc.load_default_text()
-		self.iofunc.make_environment(self.filename)
+			iofunc.load_default_text()
+		iofunc.make_environment(self.filename)
 
 		# gui main:
-		self.gui.main()
+		gui.main()
 
 
 
