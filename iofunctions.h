89a19543b53220738ce5cf0f6e5ed2a30e0d036e
azhuang <azhuang@7a9131e8-ac52-4630-8722-c3349df740a5>
2010-09-01 19:13:09 +0000

    add file declaration

--- iofunctions.h
+++ iofunctions.h
@@ -1,10 +1,27 @@
 /**
- * @file   iofunctions.h
- * @brief  
- * @author Alexander van der Mey <alexvandermey@gmail.com>
+ * @file    iofunctions.h
+ * @brief   
  *
- * Copyright (C) 2010 - Alexander van der Mey <alexvandermey@gmail.com>
+ * Copyright (C) 2010 Gummi-Dev Team <alexvandermey@gmail.com>
  * All Rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 #ifndef GUMMI_IOFUNCTIONS_H
